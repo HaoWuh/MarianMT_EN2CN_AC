a39a17781ee2798c25ea36566ff9b6d0a4584004
haowuh <wqdwh1999@126.com>
2025-01-14 14:30:05 +0800

    translate script

--- scripts/Helsinki-en2zh_translate.py
+++ scripts/Helsinki-en2zh_translate.py
@@ -6,24 +6,20 @@ from tqdm import tqdm
 from transformers import MarianMTModel, MarianTokenizer
 import torch
 
-# 加载预训练的翻译模型和分词器
-# model_path = rf"D:\Work\Shanghai\FloatingAI\pretrained_MarianMT_model" 
-# model_path = rf"C:\Work\VSCode\Python files\Shanghai\FloatingAI\EN2ZH\trained_model\model"
-model_path = rf"C:\Work\VSCode\Python files\Shanghai\FloatingAI\EN2ZH\trained_model\model_1223"
-
+# loading the trained model and tokenizer
+model_path = "../trained_model/model_1223"
 model = MarianMTModel.from_pretrained(model_path)
 tokenizer = MarianTokenizer.from_pretrained(model_path)
 
-# 检查是否有可用的 GPU
+# checking available GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-# 将模型加载到 GPU（如果可用）
 model = model.to(device)
 
-# 输入的 DataFrame 示例
-# data_path= rf"D:\Work\Shanghai\FloatingAI\大兴机场_100条\英文文语料列表1101_1130.csv"
-data_path= rf"C:\Work\VSCode\Python files\Shanghai\FloatingAI\EN2ZH\test_datasets\en2zh_test_a.csv"
+# reading demo data
+# This a demo data with both en and zh columns as original texts and labels repectively
+data_path= "../demo_data/en2zh_test.csv"
 data= pd.read_csv(data_path)
 try:
     src_text= list(data['text'])
@@ -33,31 +29,25 @@ except:
     except:
         raise Exception("No column named 'text' or 'en' in the DataFrame")
 
-# src_text= src_text[:1000]
-# src_text= src_text[:100]
 
-# 将单个文本编码为 Tensor
+# tokenize input texts
 inputs = tokenizer(src_text, return_tensors="pt", padding=True, truncation=True)
-# 使用模型生成翻译
 inputs= inputs.to(device)
 
 print("generating: ......")
 
-
-# 翻译文本并保存到文件
-output_file = "test_trained_a.csv"
+output_file = "../results/test_trained.csv"
 
 translated_texts= []
 
 for text in tqdm(src_text):
-    # 将单个文本编码为 Tensor，并移动到 GPU
+    # moving the tokenized tensors into GPU device
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-    inputs = {key: value.to(device) for key, value in inputs.items()}  # 确保所有张量在同一设备上
-    # 使用模型生成翻译
+    inputs = {key: value.to(device) for key, value in inputs.items()} 
+    # translating
     outputs = model.generate(**inputs)
-    # 将生成的结果解码为字符串
     translated = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    # 写入文件
+    # writing
     translated_texts.append(translated)
 
 save_df= pd.DataFrame({
