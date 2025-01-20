46638872c38a46207ba7d0e7e9664ae9e13ae460
HaoWuh <100411698+HaoWuh@users.noreply.github.com>
2025-01-20 15:54:27 +0800

    Update download.sh

--- trained_model/download.sh
+++ trained_model/download.sh
@@ -3,9 +3,6 @@
 # 文件夹 ID
 FOLDER_ID="1DbzEcJxkBYVuOb2MACML14SWNZveAVuH"
 
-# 创建文件夹以保存下载的文件
-mkdir -p model_20241223
-
 # 使用 gdown 下载文件夹
 gdown --folder "https://drive.google.com/drive/folders/${FOLDER_ID}" -O model_20241223/
 
